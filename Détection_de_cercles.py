e4e516a59903777abbbd6faaef0291b984ab4a1e
Zstorm999 <46458357+Zstorm999@users.noreply.github.com>
2022-10-10 15:50:35 +0200

    Friendlier text visualisation

--- Détection_de_cercles.py
+++ Détection_de_cercles.py
@@ -50,6 +50,10 @@ class ColorDetector:
         self.upper_bounds[2] = cv2.getTrackbarPos("Val Max " + self.name, self.window)
 
 
+def show_text(canvas, text: str, point: Tuple[int, int], color: Tuple[int, int, int]):
+    cv2.putText(canvas, text, point, cv2.FONT_HERSHEY_COMPLEX, 0.5, color, 1)
+
+
 def getContours(img):
     contours, _ = cv2.findContours(img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     for cnt in contours:
@@ -101,36 +105,22 @@ def choose_action(
         messageY = "OK"
         # Si le cercle est sur la cible
     if messageX == "OK" and messageY == "OK":
-        cv2.putText(
-            canvas,
-            "Cible atteinte",
-            target_point,
-            cv2.FONT_HERSHEY_COMPLEX,
-            0.5,
-            (255, 255, 255),
-            1,
-        )
+        show_text(canvas, "Cible atteinte", target_point, (255, 255, 255))
         # Affichage de la position du cercle dans l'image
-        cv2.putText(
+        show_text(
             canvas,
             "Circle Position : (" + str(circles[0, 0]) + ";" + str(circles[0, 1]) + ")",
             (canvas.shape[1] - 250, canvas.shape[0] - 6),
-            cv2.FONT_HERSHEY_COMPLEX,
-            0.5,
             (255, 255, 255),
-            1,
         )
         print("INSTRUCTIONS\tDESCENTE")
     else:
         # Affichage de la position du cercle dans l'image
-        cv2.putText(
+        show_text(
             canvas,
             "Circle Position : (" + str(circles[0, 0]) + ";" + str(circles[0, 1]) + ")",
             (canvas.shape[1] - 250, canvas.shape[0] - 6),
-            cv2.FONT_HERSHEY_COMPLEX,
-            0.5,
             color,
-            1,
         )
         cv2.circle(canvas, target_point, 10, (0, 0, 0), 2)
         # Envoi des instructions dans la console
@@ -145,10 +135,7 @@ def choose_action(
             + str(target_point[1])
             + ")",
             (1, canvas.shape[0] - 6),
-            cv2.FONT_HERSHEY_COMPLEX,
-            0.5,
             color,
-            1,
         )
 
 
@@ -190,36 +177,57 @@ def empty(a):
     pass
 
 
+class ColorCircles:
+    def __init__(self, detector: ColorDetector, display_color: Tuple[int, int, int]):
+        self.detector = detector
+        self.display_color = display_color
+        self.circles = []
+
+    def update(self):
+        self.detector.update_bounds()
+
+    def find_circles(self, hsv_image):
+        self.circles = find_circles(self.detector, hsv_image, True)
+
+    def choose_action(self, target, canvas):
+        # getContours was removed from check, need to check if it’s necessary or can be bypassed
+        if self.circles is not None:
+            choose_action(self.circles, target, canvas, self.display_color)
+
+
 def main():
     # cap = cv2.VideoCapture('http://user:pass@192.168.43.190:8000/stream.mjpg')
     cap = cv2.VideoCapture(0)
 
     # CIBLE SUR L'IMAGE PARAMETRABLE
-    targetPointX = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) // 2)
-    targetPointY = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) // 2)
+    target_point = (
+        int(cap.get(cv2.CAP_PROP_FRAME_WIDTH) // 2),
+        int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT) // 2),
+    )
 
     # Trackbar avec slider
     cv2.namedWindow("Trackbar")
     cv2.resizeWindow("Trackbar", (300, 900))
 
-    # Blue
-    blue_detector = ColorDetector("Blue", "Trackbar", (90, 50, 70), (128, 255, 255))
-
-    # Red
-    red_detector = ColorDetector("Red", "Trackbar", (159, 50, 70))
-
-    # Yellow
-    yellow_detector = ColorDetector("Yellow", "Trackbar", (25, 50, 70), (35, 255, 255))
+    # Possible blue threshold: [20, 115, 70][255, 145, 120]
+    blue_circles = ColorCircles(
+        ColorDetector("Blue", "Trackbar", (90, 50, 70), (128, 255, 255)), (255, 0, 0)
+    )
+    # No other recorded possible value for red
+    red_circles = ColorCircles(
+        ColorDetector("Red", "Trackbar", (159, 50, 70)), (0, 0, 255)
+    )
+    # Possible yellow threshold: [20, 110, 170][20, 110, 170]
+    yellow_circles = ColorCircles(
+        ColorDetector("Yellow", "Trackbar", (25, 50, 70), (35, 255, 255)), (0, 255, 255)
+    )
+    circles = [blue_circles, red_circles, yellow_circles]
 
     while True:
         # Capture frame-by-frame
         ret, image = cap.read()
         output_frame = image.copy()
         # print(h_min,h_max,s_min,s_max,v_min,v_max)
-
-        blue_detector.update_bounds()
-        red_detector.update_bounds()
-        yellow_detector.update_bounds()
 
         kernel = np.ones((5, 5), np.uint8) / 25
 
@@ -228,10 +236,7 @@ def main():
             output_frame,
             "FPS : " + str(cap.get(cv2.CAP_PROP_FPS)),
             (image.shape[1] - 100, 15),
-            cv2.FONT_HERSHEY_COMPLEX,
-            0.5,
             (255, 0, 255),
-            1,
         )
 
         # Cette fonction permet de réduire le bruit sur l'image
@@ -241,40 +246,17 @@ def main():
         image_bgr = cv2.dilate(image_bgr, kernel, iterations=5)
         cv2.imshow("erode", image_bgr)
         # On convertit dans le format HSV pour pouvoir toucher à la teinte, la saturation et la luminosité de la couleur
-        image_gray = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2GRAY)
         image_hsv = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2HSV)
 
         cv2.imshow("raw", image_hsv)
-        # Possible yellow threshold: [20, 110, 170][20, 110, 170]
-        # Possible blue threshold: [20, 115, 70][255, 145, 120]
-
-        circles_blue = find_circles(blue_detector, image_hsv, True)
-        circles_red = find_circles(red_detector, image_hsv, True)
-        circles_yellow = find_circles(yellow_detector, image_hsv, True)
 
         # On désigne le milieu de l'image comme étant la cible à atteindre
-        cv2.circle(output_frame, (targetPointX, targetPointY), 1, (0, 0, 0), 3)
-        # Message pour les instructions envoyées au drone
-        messageX, messageY = "none", "none"
+        cv2.circle(output_frame, target_point, 1, (0, 0, 0), 3)
 
-        # getContours was removed from check, need to check if it’s necessary or can be bypassed
-        if circles_red is not None:
-            choose_action(
-                circles_red, (targetPointX, targetPointY), output_frame, (0, 0, 255)
-            )
-
-        if circles_yellow is not None:
-            choose_action(
-                circles_yellow,
-                (targetPointX, targetPointY),
-                output_frame,
-                (0, 255, 255),
-            )
-
-        if circles_blue is not None:
-            choose_action(
-                circles_blue, (targetPointX, targetPointY), output_frame, (255, 0, 0)
-            )
+        for c in circles:
+            c.update()
+            c.find_circles(image_hsv)
+            c.choose_action(target_point, output_frame)
 
         cv2.imshow("frame", output_frame)
         if cv2.waitKey(1) & 0xFF == ord("q"):
