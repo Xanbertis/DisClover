d03aab90bddfd48b0caa3e836f80ee17c8e187b1
Zstorm999 <thomasarchambeau@outlook.fr>
2023-02-01 11:47:14 +0100

    Started working on zone detect

---
+++ zone_detection.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+from time import perf_counter
+from dataclasses import dataclass
+from typing import Tuple
+
+from tqdm import tqdm
+
+
+from skimage import exposure
+
+from utils import downscale, empty, show_text
+
+
+def main():
+    ...
+
+
+if __name__ == "__main__":
+    main()
